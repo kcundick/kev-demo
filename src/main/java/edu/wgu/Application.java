a64266cb12d0a63834031c155320a768860173b4
Kevin Cundick <kcundick>
2021-08-05 00:06:58 +0000

    Initial commit

---
+++ src/main/java/edu/wgu/Application.java
@@ -0,0 +1,10 @@
+package edu.wgu;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
